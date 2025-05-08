9fd7fdfffbd81fe52d34cba1c38555cb5d72eedc
arul4902 <kumarsiva42887@gmail.com>
2025-05-08 22:29:18 +0530

    update-2

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+
+// middleware.js (or .ts)
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -5,14 +7,9 @@ const isProtectedRoute = createRouteMatcher([
   '/forum(.*)',
 ]);
 
-// If running on Netlify, disable middleware
-const isNetlify = process.env.NETLIFY === 'true';
-
-export default isNetlify
-  ? () => {}
-  : clerkMiddleware((auth, req) => {
-      if (isProtectedRoute(req)) auth().protect();
-    });
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
